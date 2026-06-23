2357b76e6633ce32b2564201799b65143bb9b0c7
MRM3UK <mynewp570@gmail.com>
2026-06-23 08:17:10 +0600

    Add configuration settings for the bot

---
+++ config.py
@@ -0,0 +1,25 @@
+# config.py
+
+BOT_TOKEN = "8522132595:AAF3lg5RbcnwHIKCW9eJQHFBCB5JAOakAlY"
+ADMIN_ID = 6842614698
+LOG_CHANNEL_ID = -1002603018433
+
+# Supported platforms
+SUPPORTED_PLATFORMS = [
+    "instagram.com",
+    "facebook.com",
+    "fb.watch",
+    "tiktok.com",
+    "twitter.com",
+    "x.com",
+    "pinterest.com",
+    "pin.it",
+    "snapchat.com",
+    "story.snapchat.com"
+]
+
+# Max file size for Telegram (50MB)
+MAX_FILE_SIZE = 50 * 1024 * 1024
+
+# Database file
+DB_FILE = "bot_database.db"
